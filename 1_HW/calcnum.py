ea095d88a3940025dfd00f97e5fb3bd6dea5102b
Row35 <ivan.shtoyko@gmail.com>
2020-02-09 19:40:48 +0200

    1st hw files were added

---
+++ 1_HW/calcnum.py
@@ -0,0 +1,7 @@
+a=int(input("insert a = "))
+b=int(input("insert b = "))
+
+print ("sum = ", a+b)
+print ("a-b = ", a-b)
+print ("a/b = ",a/b)
+print ("a*b = ",a*b)
---
+++ 1_HW/userbot.py
@@ -0,0 +1,12 @@
+"""
+Hello, (answer(name))“, “Your age is  (answer(age))“, “You live in  (answer(city)) 
+"""
+user_name = (input("What is your name? "))
+user_age = int(input("How old are you? "))
+user_location = (input("Where do you live? "))
+
+print ("You live in,",user_location)
+print ("Hello,",user_name)
+print ("Your age is,",user_age)
+
+
